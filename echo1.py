1d26a72142c005783e27957d19ebdcd23d92c31a
BodyaSigma <162556635+BodyaSigma@users.noreply.github.com>
2024-04-19 10:23:39 +0300

    Update echo1.py

--- echo1.py
+++ echo1.py
@@ -1,10 +1,10 @@
 
-import telebot
+import telebot #Импортируем библиотеку.
 
-bot = telebot.TeleBot('7160501552:AAFmjrBE3XhbyucKrWDJLEKLaHR0RNuUfT0')
+bot = telebot.TeleBot('7160501552:AAFmjrBE3XhbyucKrWDJLEKLaHR0RNuUfT0') #Подключаемся к боту.
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=["text"])  #Считывание сообщения.
+def repeat_all_messages(message): #Функция для этого сообщения.
+    bot.send_message(message.chat.id, message.text) #Функция отправки сообщения
 
-bot.polling(True)
+bot.polling(True) #Команда для того чтобы Бот Работал.
